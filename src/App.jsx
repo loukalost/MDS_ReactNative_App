792c7cb3ed84de8d194984088a39111c3f38d17f
Louka Lemonnier <lk.lemonnier@gmail.com>
2025-02-18 14:48:02 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react'
+import {
+  SafeAreaView,
+  ScrollView,
+  View,
+  Text
+} from 'react-native'
+
+function App () {
+  return (
+    <SafeAreaView>
+      <ScrollView contentInsetAdjustmentBehavior='automatic'>
+        <View>
+          <Text>COUCOU (Miss Jirachi)</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+export default App
